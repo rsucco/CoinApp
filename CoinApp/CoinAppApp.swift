8d5713b8f9c52d69c7253b3d7acd3e5db013402d
Ryan Succo <rsucco@live.com>
2021-07-11 20:12:34 -0400

    Initial Commit

---
+++ CoinApp/CoinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinAppApp.swift
+//  CoinApp
+//
+//  Created by Ryan Succo on 11Jul21.
+//
+
+import SwiftUI
+
+@main
+struct CoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoinApp
+//
+//  Created by Ryan Succo on 11Jul21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
